2ebabcf20bc76a7f444c4033fabf58cfae619c28
Fernando Serrano San Martin <fernando.san@softtek.com>
2024-09-19 15:35:53 +0200

    saco datos de las personas de about , ngfor , firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,10 +10,12 @@ export class InfoPaginaService {
   loading:boolean=true;
   info:InfoPagina={};
 
+  equipo:any[]=[];
+
   constructor( private http:HttpClient) {
 
       this.getDataGlobal();
-      
+      this.getDataEquipo();
   }
     
   getDataGlobal(){
@@ -24,11 +26,28 @@ export class InfoPaginaService {
       this.http.get('../../assets/data/db.json')
       .subscribe(
         (data:InfoPagina) => {
-            console.log(data)
+            // console.log(data)
             this.info = data;
             this.loading = false;
         }
-      )
-   
+      )   
+  }
+
+  getDataEquipo(){
+
+    this.loading = true;
+    console.log("llamando al servicio");
+
+    this.equipo = []
+    this.http.get('https://portafolio-665c0-default-rtdb.europe-west1.firebasedatabase.app/equipo.json')
+    .subscribe(
+      (data:any) => {   
+          this.equipo = data;
+          this.loading = false;
+      }
+    )   
+
+
+
   }
 }
