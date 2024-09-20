1b26073143f8a55276ac1d98f60c5de5db6a60d4
Fernando Serrano San Martin <fernando.san@softtek.com>
2024-09-20 12:50:12 +0200

    arreglando cosillas de imagenes..

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -24,10 +24,10 @@ export class ItemComponent {
     this._route.paramMap
       .pipe(
         tap((data:any)=>{
-          this.imgProduct = 'assets/img/'+data.params.id+'.jpg'
-          this.imgProduct2 = 'assets/img/'+data.params.id+'/pic-1.jpg'
-          this.imgProduct3 = 'assets/img/'+data.params.id+'/pic-2.jpg'
-          this.photo       = 'assets/img/'+data.params.id+'/main.jpg'
+          this.imgProduct = '../../../../assets/img/'+data.params.id+'.jpg'
+          this.imgProduct2 = '../../../../assets/img/'+data.params.id+'/pic-1.jpg'
+          this.imgProduct3 = '../../../../assets/img/'+data.params.id+'/pic-2.jpg'
+          this.photo       = '../../../../assets/img/'+data.params.id+'/main.jpg'
         }),
         switchMap(
           (data:any)=> _dataProducts.getDataAllProd(data.params.id)
